60d18515e72d4c53ddd1b7bfaadd1a50da80f07a
Vladislav Arutiunian <vladislav.arutiunian@gmail.com>
2022-12-24 01:48:51 +0300

    3 step basic gendiff realized

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/VladislavArutiunian/php-project-48/workflows/hexlet-check/badge.svg)](https://github.com/VladislavArutiunian/php-project-48/actions)
+
+# Gendiff Hexlet Project
+
+[![asciicast](https://asciinema.org/a/70erEsdwtfXObfxZAUNmPerYB.svg)](https://asciinema.org/a/70erEsdwtfXObfxZAUNmPerYB)
---
+++ src/genDiff.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Hexlet\Code\gendiff;
+
+use function cli\line;
+
+function genDiff(string $firstPath, string $secondPath, string $format = 'stylish'): string
+{
+    $firstColl = json_decode(file_get_contents($firstPath), true);
+    $secondColl = json_decode(file_get_contents($secondPath), true);
+
+    $res = [];
+    foreach ($firstColl as $key => $value) {
+        $value = normalize($value);
+        if (array_key_exists($key, $secondColl)) {
+            $value2 = normalize($secondColl[$key]);
+            $str = ($value === $value2) ? '    ' . "$key: $value\n" : "  - $key: $value\n  + $key: $value2\n";
+        } else {
+            $str = "  - $key: $value\n";
+        }
+        $res[] = $str;
+    }
+    foreach ($secondColl as $key => $value) {
+        if (!array_key_exists($key, $firstColl)) {
+            $normalizedValue = (is_bool($value)) ? var_export($value, 1) : $value;
+            $res[] = "  + $key: $normalizedValue\n";
+        }
+    }
+    $compare = function ($left, $right) use ($res) {
+        $leftValue = $res[$left];
+        $rightValue = $res[$right];
+        $filter = '+- ';
+        $normalizedLeft = ltrim($leftValue, $filter);
+        $normalizedRight = ltrim($rightValue, $filter);
+        return $normalizedLeft[0] <=> $normalizedRight[0];
+    };
+    uksort($res, $compare);
+    return "{\n" . implode('', $res) . "}";
+}
+
+function normalize(bool|string $value): string
+{
+    return (is_bool($value)) ? var_export($value, 1) : $value;
+}
